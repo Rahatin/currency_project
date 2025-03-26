07719b8f3ad7d503c2d99a5bf34d2f399ed78158
Qistarah <umayerrifat@gmail.com>
2025-03-27 00:52:26 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function addCurrency() {
+    alert('Add Currency clicked');
+}
+
+function publish() {
+    alert('Publish clicked');
+}
+
+// if(hasEmpty){
+//     form.classList.remove('secActive');
+//     alert("Input is empty");
+// } else {
+//     form.classList.add('secActive');
+//     // Redirect to Page 2
+//     window.location.href = "page2.html"; // change "page2.html" to your actual file name
+// }
+// const form = document.querySelector("form"),
+//       publish = document.querySelector(".publish"),
+//       addCurrency = document.querySelector(".addCurrency"),
+//       allInput = document.querySelectorAll(".first input"); // assuming it's .first not .frist
+
+// addCurrency.addEventListener("click", () => {
+//     let hasEmpty = false;
+
+//     allInput.forEach(input => {
+//         if(input.value === ""){
+//             hasEmpty = true;
+//         }
+//     });
+
+//     if(hasEmpty){
+//         form.classList.remove('secActive');
+//         alert("Input is empty");
+//     } else {
+//         form.classList.add('secActive');
+//         window.location.href = "edit-currency.html"; // your second page name here
+//     }
+// });
